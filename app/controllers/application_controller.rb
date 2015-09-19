cab891be7e47affad4756309300b6372ec6b8d6c
Jess Jacobs <akisma@gmail.com>
2015-09-19 15:45:19 -0400

    add CORS policies to headers, we are go

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,39 @@
+# The following attempt to fix CORS was made by Jess after reading:
+# http://stackoverflow.com/questions/17858178/allow-anything-through-cors-policy
+# https://github.com/cleor41/Cors-Rails4-API
+
+# although, this seems to be more in line with what we need, since this one is for Rails v4
+# https://gist.github.com/dhoelzgen/cd7126b8652229d32eb4
+
+# it works, but I have no idea if it's best practice or not.
+
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :cors_preflight_check
+  after_filter :cors_set_access_control_headers
+
+  # For all responses in this controller, return the CORS access control headers.
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  end
+
+  # If this is a preflight OPTIONS request, then short-circuit the
+  # request, return only the necessary headers and return an empty
+  # text/plain.
+
+  def cors_preflight_check
+    if request.method == :options
+      headers['Access-Control-Allow-Origin'] = '*'
+      headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+      headers['Access-Control-Request-Method'] = '*'
+      headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
+      render :text => '', :content_type => 'text/plain'
+    end
+  end
 end
